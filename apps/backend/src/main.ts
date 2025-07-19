b8942c17c80da6d7970b8f6ec84c06169db10b06
Adel srour <adelmalys@hotmail.com>
2025-07-19 14:03:59 +0300

    chore: install and apply helmet middleware to protect against XSS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,11 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { rateLimitMiddleware } from './middlewares/rate-limit.middleware';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  // Use Helmet middleware for security headers
+  app.use(helmet());
 
   // Use the rate limit middleware
   app.use(rateLimitMiddleware);
